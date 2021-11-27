371ecf345294475e02ee44209a7e08e3317d1c17
Aaron8907 <85762827+Aaron8907@users.noreply.github.com>
2021-11-26 22:05:46 -0600

    Updated api.js and server.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -15,8 +18,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useFindAndModify: false
 });
 
-// routes
-app.use(require("./routes/api.js"));
+
+app.use(require("./routes/api"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
